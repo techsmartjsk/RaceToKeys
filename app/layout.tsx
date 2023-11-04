c382310ab8c0ab6133ba8506c49f947d501a320b
techsmartjsk <techsmartjsk@gmail.com>
2023-11-04 17:22:51 +0530

    Session

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Sidebar from '@/components/sidebar/sidebar'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/authentication/auth'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,11 +12,14 @@ export const metadata: Metadata = {
   description: 'Network by Overmind',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const session = await getServerSession(authOptions);
+  console.log(session)
+
   return (
     <html lang="en">
       <body className={inter.className}>
