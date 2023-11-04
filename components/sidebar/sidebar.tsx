1307b6f4abb8f16c56964c987b2d92a1463d1bf4
techsmartjsk <techsmartjsk@gmail.com>
2023-11-04 17:03:16 +0530

    Dashboard

---
+++ components/sidebar/sidebar.tsx
@@ -0,0 +1,7 @@
+export default function Sidebar(){
+    return(
+        <div className="bg-black">
+            <h1 className="uppercase text-white font-bold">Network</h1>
+        </div>
+    )
+}
