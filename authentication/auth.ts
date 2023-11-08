3868cb2a4845e551c9341e9cee5c1156c4dd87f1
techsmartjsk <techsmartjsk@gmail.com>
2023-11-08 23:01:57 +0530

    PostgreSQL setup

--- authentication/auth.ts
+++ authentication/auth.ts
@@ -5,13 +5,74 @@ import { NextAuthOptions, getServerSession } from 'next-auth';
 import TwitterProvider, { TwitterProfile } from 'next-auth/providers/twitter';
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     TwitterProvider({
       clientId: process.env.TWITTER_ID as string,
       clientSecret: process.env.TWITTER_SECRET as string,
       version:'2.0',
+      profile(profile: TwitterProfile){
+        const wallet = new AptosAccount();
+        const privateKey = wallet.toPrivateKeyObject().privateKeyHex;
+
+        return {
+          id: profile.data.id,
+          username: profile.data.username,
+          name: profile.data.name ?? '',
+          image: profile.data.profile_image_url ?? '',
+          privateKey: privateKey,
+        };
+      },
     }),
   ],
+  callbacks:{
+    session({ session, user }) {
+      return {
+        ...session,
+        user: {
+          id: user.id,
+          username: user.username,
+          privateKey: user.privateKey,
+          name: user.name,
+          image: user.image,
+        },
+      };
+    },
+  },
+  events: {
+    async signIn({ account }) {
+      try {
+        if (account) {
+          const foundAccount = await prisma.account.findUnique({
+            where: {
+              provider_providerAccountId: {
+                provider: 'twitter',
+                providerAccountId: account.providerAccountId,
+              },
+            },
+          });
+
+          if (foundAccount) {
+            await prisma.account.update({
+              where: {
+                provider_providerAccountId: {
+                  provider: 'twitter',
+                  providerAccountId: account.providerAccountId,
+                },
+              },
+              data: {
+                refreshToken: account.refresh_token,
+                accessToken: account.access_token,
+                expiresAt: account.expires_at,
+              },
+            });
+          }
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
   secret: process.env.NEXTAUTH_SECRET
 };
 
---
+++ next-auth.d.ts
@@ -0,0 +1,17 @@
+import { DefaultUser } from 'next-auth';
+
+export type { Session } from 'next-auth';
+
+declare module 'next-auth' {
+  interface Session {
+    user: User;
+  }
+
+  interface User extends Omit<DefaultUser, 'id'> {
+    id: PrismaUser['id'];
+    username: PrismaUser['username'];
+    name: PrismaUser['name'];
+    image: PrismaUser['image'];
+    privateKey: PrismaUser['privateKey'];
+  }
+}
