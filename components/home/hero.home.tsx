27c8ade13d821e034d1d4872e1276d8d80dee4f4
techsmartjsk <techsmartjsk@gmail.com>
2023-11-10 23:04:44 +0530

    Added Trade History

--- components/home/hero.home.tsx
+++ components/home/hero.home.tsx
@@ -1,13 +1,37 @@
-import { getTradeHistory } from "@/lib/contract";
-import { Session } from "@/lib/types"
+import { getOwnedCollections, getProtocolFeePercentage, getSubjectFeePercentage, getTradeHistory } from "@/lib/contract";
+import { ContractGetOwnedCollectionsResponse, ContractTradeEvent, Session } from "@/lib/types"
+import Image from "next/image"
 
 const Hero = async ({ session }: { session: Session }) => {
-    const tradeHistory = await getTradeHistory();
+    const tradeHistory: ContractTradeEvent[] = await getTradeHistory();
+    const protocolPercentage = await getProtocolFeePercentage();
+    const subjectPercentage = await getSubjectFeePercentage();
+    const ownedKeys = await getOwnedCollections(session.user)
 
     return (
-        <div className="px-20 py-10">
-            <h1 className="font-bold text-xl">Your Keys</h1>
-            <h1 className="text-xl">Trade History</h1>
+        <div className="px-20 py-10 w-full">
+            <h1 className="text-xl">Dashboard</h1>
+            <div className="flex gap-10 mt-10 justify-center">
+                <div className="w-1/2">
+                    <p className="text-7xl text-[#30D5C8] font-bold">{protocolPercentage}%</p>
+                    <p className="text-xl">Protocol Fee Percentage</p>
+                </div>
+                <div className="w-1/2">
+                    <p className="text-7xl text-[#30D5C8] font-bold">{subjectPercentage}%</p>
+                    <p className="text-xl">Subject Fee Percentage</p>
+                </div>
+            </div>
+            <h1 className="text-xl mt-10">Your Keys</h1>
+            {
+                ownedKeys.length <= 0 ? <div className="flex justify-center w-full">
+                <Image 
+                src="/assets/svgs/not_found.svg" 
+                width={400} 
+                height={400} 
+                alt="Keys not found"/>
+                    </div>:null
+            }
+            <h1 className="text-xl mt-20">Trade History</h1>
             <div className="rounded-md w-full shadow-md p-5 mt-10">
                 <div className="flex gap-10 border-b-[0.5px] border-black p-2">
                     <p className="w-[20%]">Sequence Number</p>
